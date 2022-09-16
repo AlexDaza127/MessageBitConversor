3ffc8c20150b7c18e1872a32409b994a3bc72b18
AlexDaza127 <michaeldaza90@gmail.com>
2022-09-15 23:21:57 -0500

    Inicio proyecto MessageBitConversor

---
+++ src/main/java/compound/MessageBitConversor.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package compound;
+
+/**
+ * @author Michael
+ *
+ */
+public class MessageBitConversor {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
